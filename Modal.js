71e81a1a5d323a4ed9133e549de3d7d05d9e0583
yogesh-85 <157303605+yogesh-85@users.noreply.github.com>
2024-04-27 15:22:20 +0530

    Created a Modal for greater look

---
+++ Modal.js
@@ -0,0 +1,32 @@
+import {Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import classes from '../UI/Modal.module.css'
+
+
+const Backdrop = (props) => {
+    return (
+    <div className={classes.backdrop}></div>
+)
+}
+const ModelOverlay = (props) => {
+    return (
+        <div className={classes.modal}>
+        <div className={classes.content}>{props.children}</div>
+        </div> 
+    )
+}
+const portalElement = document.getElementById('overlays')
+const Modal = props => {
+    return(
+        <Fragment>
+            {ReactDOM.createPortal(<Backdrop/>, portalElement)}
+            {ReactDOM.createPortal(<ModelOverlay>{props.children}</ModelOverlay>,portalElement)}
+             
+
+        </Fragment>
+
+    )
+
+
+}
+export default Modal
