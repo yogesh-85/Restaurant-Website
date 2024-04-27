d34ec67aba135c5ea7f523f2bceb07e6bd8680db
yogesh-85 <157303605+yogesh-85@users.noreply.github.com>
2024-04-27 16:20:38 +0530

    Updated header

--- header.js
+++ header.js
@@ -1,25 +1,21 @@
 import {Fragment} from 'react';
+import HeaderCartButton from '../Layout/HeaderCartButton'
 import image from '../../assests/meals.jpg';
 import classes from '../Layout/header.module.css';
 
-
-
-
-const Header = () => {
+const Header = (props) => {
     return(
         <Fragment>
             <header className={classes.header}>
                 <h1>
                     React meals
                 </h1>
-                <button>
-                    Cart
-                </button>
+                <HeaderCartButton onClick={props.onShowCart}/>
+              </header>
                 <div className={classes['main-image']}>
                     <img src={image} alt='A tabel full of delicious food'></img>
 
                 </div>
-            </header>
         </Fragment>
     )
 }
